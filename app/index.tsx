6b8d44df4d4159634e0b0d942905b04bc282e52f
mrhanna <hanna.michaelr@gmail.com>
2025-08-06 07:11:35 -0500

    Added Nativewind and Prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import '../global.css';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
