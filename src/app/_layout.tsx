73a3a4d2370c5e9b2c25bceb820e96b87dbb49bd
Michael Hanna <hanna.michaelr@gmail.com>
2025-08-07 22:42:02 -0500

    providing state to app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import '@/global.css';
 import { Stack } from 'expo-router';
+import { Provider } from 'react-redux';
+import { store } from '../state/store';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack />;
+    </Provider>
+  );
 }
