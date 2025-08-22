7f78c1795ca0e95c061eba667484d0fbdcb7315f
mrhanna <hanna.michaelr@gmail.com>
2025-08-22 01:43:27 -0500

    Progress bar animates

--- src/components/GameScreen/ProgressBar.tsx
+++ src/components/GameScreen/ProgressBar.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function ProgressBar({
   value,
@@ -7,12 +14,22 @@ export default function ProgressBar({
   value: number;
   max: number;
 }) {
+  const sharedValue = useSharedValue(value);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: `${(Math.max(0, sharedValue.value) / max) * 100}%`,
+  }));
+
+  useEffect(() => {
+    sharedValue.value = withTiming(value, {
+      duration: 400,
+      easing: Easing.inOut(Easing.exp),
+    });
+  }, [value]);
+
   return (
     <View className="h-2 w-full bg-gray-200">
-      <View
-        className="h-full bg-blue-500"
-        style={{ width: `${(value / max) * 100}%` }}
-      />
+      <Animated.View className="h-full bg-blue-500" style={animatedStyle} />
     </View>
   );
 }
